f1ab3f6b952f41a04677d163c000305dc7691126
Roger <roger.sama@gmail.com>
2018-07-19 23:29:12 -0300

    Inicio da sidebar

--- dist/darktropical/js/custom.js
+++ dist/darktropical/js/custom.js
@@ -1,14 +1,16 @@
 $( document ).ready(function() {
-    $(".form-group").focusin(function(){
+
+    $(".form-group").on("focusin", function(){
         $(this).addClass("form-group-focused");
         $(this).children("label").addClass("label-outside-form");
     });
 
-    $(".form-group").focusout(function(){
+    $(".form-group").on("focusout", function(){
         $(this).removeClass("form-group-focused");
 
         if( !$(this).children("input").val() ) {
             $(this).children("label").removeClass("label-outside-form");
         }
     });
+    
 });
